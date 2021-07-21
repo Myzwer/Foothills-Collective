51861fe4bbf4e0cffbcff11f0f127105a37e074f
Adrian Garner <acgarner11@yahoo.com>
2021-07-21 15:59:55 -0400

    :rocket: Added the new working footer. It works on mobile and desktop.

--- footer.php
+++ footer.php
@@ -9,45 +9,33 @@
  * @package Collective
  */
 ?>
-<footer class="bg-black text-white pt-12 pb-8 px-4">
-    <div class="container overflow-hidden flex flex-col lg:flex-row justify-between mx-auto px-4 hoverfooter">
-        <div class="w-3/4 block sm:flex text-sm mt-6 lg:mt-0">
-            <ul class="text-gray-700 list-none p-0 font-thin flex flex-col text-left w-full">
-                <li class="inline-block py-2 px-3 text-white uppercase font-medium tracking-wide">Product</li>
-                <li><a href="#" class="inline-block py-2 pl-3 pr-5 text-gray-500 hover:text-charcoal no-underline">Features</a></li>
-                <li><a href="#" class="inline-block py-2 pl-3 pr-5 text-gray-500 hover:text-charcoal no-underline">Integrations</a></li>
-                <li><a href="#" class="inline-block py-2 pl-3 pr-5 text-gray-500 hover:text-charcoal no-underline">Pricing</a></li>
-                <li><a href="#" class="inline-block py-2 pl-3 pr-5 text-gray-500 hover:text-charcoal no-underline">FAQ</a></li>
-            </ul>
-            <ul class="text-gray-700 list-none p-0 font-thin flex flex-col text-left w-full">
-                <li class="inline-block py-2 px-3 text-white uppercase font-medium tracking-wide">Company</li>
-                <li><a href="#" class="inline-block py-2 pl-3 pr-5 text-gray-500 hover:text-charcoal no-underline">Privacy</a>
-                </li>
-                <li><a href="#" class="inline-block py-2 pl-3 pr-5 text-gray-500 hover:text-charcoal no-underline">Terms of Service</a></li>
-            </ul>
-            <ul class="text-gray-700 list-none p-0 font-thin flex flex-col text-left w-full">
-                <li class="inline-block py-2 px-3 text-white uppercase font-medium tracking-wide">Social Media</li>
-                <li><a href="#" class="inline-block py-2 pl-3 pr-5 text-gray-500 hover:text-charcoal no-underline">Facebook</a></li>
-                <li><a href="#" class="inline-block py-2 pl-3 pr-5 text-gray-500 hover:text-charcoal no-underline">Instagram</a></li>
-                <li><a href="#" class="inline-block py-2 pl-3 pr-5 text-gray-500 hover:text-charcoal no-underline">YouTube</a></li>
-                </li>
-            </ul>
-            <div class="text-gray-700 flex flex-col w-full">
-                <div class="flex pl-4 justify-start mt-2">
-                    <a class="block flex items-center text-gray-300 hover:text-charcoal mr-6 no-underline" href="#">
-                        <svg viewBox="0 0 24 24" class="fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg">
-                    </a>
-                    <a class="block flex items-center text-gray-300 hover:text-charcoal no-underline" href="#">
-                        <svg viewBox="0 0 24 24" class="fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg">
-
-                    </a>
-                </div>
-            </div>
+<footer>
+    <div class="bg-black grid grid-cols-12 uppercase pt-5 md:pl-60">
+        <div class="col-span-8 md:col-span-2 mx-4">
+        <ul class="pb-5">
+            <li class="text-gray text-xs pb-3 md:pb-4">Who We Are</li>
+            <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Home</a></li>
+            <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">About</a></li>
+            <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Foothills Church</a></li>
+        </ul>
         </div>
-    </div>
-    <div class="pt-4 mt-4 pt-6 text-gray-600 border-t border-gray-800 flex flex-col md:flex-row justify-center items-center">
-        <div>© 2021 Foothills Collective</div>
-        <div>&nbsp;All rights reserved.</div>
+        <div class="col-span-8 md:col-span-2 mx-4">
+            <ul class="pb-5">
+                <li class="text-gray text-xs pb-3 md:pb-4">useful links</li>
+                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Music</a></li>
+                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Merch</a></li>
+                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Contact</a></li>
+                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Privacy Policy</a></li>
+            </ul>
+        </div>
+        <div class="col-span-8 md:col-span-2 mx-4">
+            <ul class="">
+                <li class="text-gray text-xs pb-3 md:pb-4">LET'S BE SOCIAL</li>
+                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">facebook</a></li>
+                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">instagram</a></li>
+                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">youtube</a></li>
+            </ul>
+        </div>
     </div>
 </footer>
 
