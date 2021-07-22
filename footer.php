57cad9445f3919e5e38d1ae139456d0e2b113103
Adrian Garner <acgarner11@yahoo.com>
2021-07-22 10:38:29 -0400

    :construction: Added dynamically generated links in the footer.

--- footer.php
+++ footer.php
@@ -13,31 +13,28 @@
     <div class="bg-black grid grid-cols-12 uppercase pt-5 md:pl-60">
         <div class="col-span-8 md:col-span-2 mx-4">
         <ul class="pb-5">
-            <li class="text-gray text-xs pb-3 md:pb-4">Who We Are</li>
-            <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Home</a></li>
-            <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">About</a></li>
-            <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Foothills Church</a></li>
+            <?php
+            wp_nav_menu(array ('theme_location' => 'footer-column-1'));
+            ?>
         </ul>
         </div>
         <div class="col-span-8 md:col-span-2 mx-4">
             <ul class="pb-5">
-                <li class="text-gray text-xs pb-3 md:pb-4">useful links</li>
-                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Music</a></li>
-                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Merch</a></li>
-                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Contact</a></li>
-                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">Privacy Policy</a></li>
+                <?php
+                wp_nav_menu(array ('theme_location' => 'footer-column-2'));
+                ?>
             </ul>
         </div>
         <div class="col-span-8 md:col-span-2 mx-4">
             <ul class="">
-                <li class="text-gray text-xs pb-3 md:pb-4">LET'S BE SOCIAL</li>
-                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">facebook</a></li>
-                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">instagram</a></li>
-                <li class="text-white text-xs pb-3 md:pb-4"><a class="hover:text-charcoal transition duration-200 ease-in-out" href="#">youtube</a></li>
+                <?php
+                wp_nav_menu(array ('theme_location' => 'footer-column-3'));
+                ?>
             </ul>
         </div>
     </div>
 </footer>
+
 
 
 
