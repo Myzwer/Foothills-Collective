7727f23714dbcdad5feb05bf65f4a4f17b567eac
Adrian Garner <acgarner11@yahoo.com>
2021-07-22 12:20:04 -0400

    :art: Made the the title of the links a different color than the links.

--- footer.php
+++ footer.php
@@ -12,26 +12,31 @@
 <footer class="footer">
     <div class="bg-black grid grid-cols-12 uppercase pt-5 md:pl-60 footer-contain">
         <div class="col-span-8 md:col-span-2 mx-4 footer-col">
-        <ul class="pb-5">
+        <ul class="pb-5 section-title">
+            <a>
             <?php
             wp_nav_menu(array ('theme_location' => 'footer-column-1'));
-            ?>
+            ?></a>
         </ul>
         </div>
         <div class="col-span-8 md:col-span-2 mx-4 footer-col">
-            <ul class="pb-5">
+            <ul class="pb-5 section-title">
+                <a>
                 <?php
                 wp_nav_menu(array ('theme_location' => 'footer-column-2'));
-                ?>
+                ?></a>
             </ul>
         </div>
-        <div class="col-span-8 md:col-span-2 mx-4 footer-col>
-            <ul class="">
+        <div class="col-span-8 md:col-span-2 mx-4 footer-col">
+            <ul class="section-title">
+                <a>
                 <?php
                 wp_nav_menu(array ('theme_location' => 'footer-column-3'));
                 ?>
+                </a>
             </ul>
         </div>
+    <div class="clearfix"></div>
     </div>
 </footer>
 
