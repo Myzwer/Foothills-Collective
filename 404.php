3e2c99d5947c0c4fed8b169079bb5dfff2015d3d
Adrian Garner <acgarner11@yahoo.com>
2021-07-27 11:21:00 -0400

    :art: Added the title and its features.

---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ * Page design by: https://codepen.io/johnfinkdesign/pen/XjkBPE?editors=1100
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package wordpack-theme
+ */
+
+get_header();
+?>
+
+    <div class="error-page">
+        <div>
+            <h1 data-h1="404">404</h1>
+            <p data-p="NOT FOUND">NOT FOUND</p>
+            <p> </p>
+        </div>
+    </div>
+    <a class="back" href="#">GO BACK</a>
+
+
+<?php
+get_footer();
