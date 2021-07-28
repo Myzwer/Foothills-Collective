a5b4e9c1a1058997bc54485be030d0fb26d204bb
Adrian Garner <acgarner11@yahoo.com>
2021-07-28 09:54:30 -0400

    :white_check_mark: Added about us page.

--- about.php
+++ about.php
@@ -25,18 +25,18 @@ get_header();
             </div>
         </div>
 <div class="mx-5 md:mx-64">
-        <h1 class="text-center text-2xl md:text-7xl pt-5 md:pb-8 md:pt-10 "><?php the_field('title'); ?></h1>
-        <hr class="md:pb-8 ">
-        <h2 class="text-lg md:text-3xl text-center leading-6 pt-3 pb-10 md:pb-20 "><?php the_field('subtitle'); ?></h2>
+        <h1 class="text-center font-bold text-xl md:text-6xl pt-5 md:pb-8 md:pt-10 "><?php the_field('title'); ?></h1>
+        <hr class="md:pb-8 opacity-20">
+        <h2 class="text-lg md:text-3xl text-center leading-6 pt-3 pb-10 "><?php the_field('subtitle'); ?></h2>
 
-        <p class="font-bold pb-3 md:pb-1 "><?php the_field('bodyhead-1'); ?></p>
-        <p class="pb-8 md:pb-16 "><?php the_field('body-1'); ?></p>
+        <p class="font-bold pb-1"><?php the_field('bodyhead-1'); ?></p>
+        <p class="pb-8  "><?php the_field('body-1'); ?></p>
 
-        <p class="font-bold pb-3 md:pb-1"><?php the_field('bodyhead-2'); ?></p>
-        <p class="pb-8 md:pb-16"><?php the_field('body-2'); ?></p>
+        <p class="font-bold pb-1"><?php the_field('bodyhead-2'); ?></p>
+        <p class="pb-8 "><?php the_field('body-2'); ?></p>
 
-        <p class="font-bold pb-3 md:pb-1"><?php the_field('bodyhead-3'); ?></p>
-        <p class="pb-8 md:pb-16"><?php the_field('body-3'); ?></p>
+        <p class="font-bold pb-1"><?php the_field('bodyhead-3'); ?></p>
+        <p class="pb-8  "><?php the_field('body-3'); ?></p>
 </div>
     </div>
 
