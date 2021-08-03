459bb1dcdf1bb4d195b8ef33cbff1108ba873eca
Adrian Garner <acgarner11@yahoo.com>
2021-08-03 10:58:56 -0400

    :heavy_plus_sign: Added Release page and its css page.

---
+++ release.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Release
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ *
+ * @package WordPress
+ * @subpackage Wordpack Theme
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+    <!--Header image-->
+    <div class="content">
+        <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+      rgba(0, 0, 0, 0.45),
+      rgba(0, 0, 0, 0.45)
+    ), url('http://foothills-collective.local/wp-content/uploads/2021/08/Collective-Black-And-White.jpg');
+     background-position: center center;
+     height: 100vh;">
+            <div class=" lg:mx-40 p-5 bg-white h-40 w-full content-middle">
+                <div class=" text-white ">
+                    <img class="release-profile rounded-full" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80">
+            </div>
+            </div>
+
+    </div>
+    </div>
+
+
+
+<?php get_footer();
