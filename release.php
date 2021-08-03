96b2e09a4bbbf1883e63f51cc6fbf210531580a8
Adrian Garner <acgarner11@yahoo.com>
2021-08-03 14:15:29 -0400

    :art: Added the top section on desktop and mobile.

--- release.php
+++ release.php
@@ -22,8 +22,19 @@ get_header(); ?>
      height: 100vh;">
 <div class="pt-20">
 <div class="grid grid-cols-12">
-            <div class=" lg:mx-40 bg-white p-5 col-span-12">
-                    <img class="h-32 w-32 rounded-full mx-auto -mt-20" src="http://foothills-collective.local/wp-content/uploads/2021/08/Collective-Profile-Photo.jpg">
+            <div class=" lg:mx-40 bg-white p-5 col-span-12 rounded-lg mx-5">
+                <img class="h-32 w-32 rounded-full mx-auto -mt-20" src="http://foothills-collective.local/wp-content/uploads/2021/08/Collective-Profile-Photo.jpg">
+           <h2 class="text-center pt-5 text-2xl font-bold">Foothills Collective</h2>
+                <h2 class="text-center pt-2"><i class="fas fa-map-marker-alt pr-1"></i> Knoxville, TN</h2>
+                <h2 class="text-center md:mx-80 pt-3">We are a collection of artists, musicians, songwriters, and producers out of Foothills Church with the desire to create music that connects people to Jesus.</h2>
+
+                    <div class="text-center pt-5">
+                <i class="fab fa-instagram bg-black md:p-5 p-3 rounded-full text-white"></i>
+                <i class="fab fa-facebook-f bg-black md:px-6 md:py-5 px-4 py-3 rounded-full text-white"></i>
+                <i class="fab fa-youtube bg-black md:p-5 p-3 rounded-full text-white"></i>
+                <i class="fab fa-spotify bg-black md:p-5 p-3 rounded-full text-white"></i>
+                <i class="fas fa-music bg-black md:p-5 p-3 rounded-full text-white"></i>
+                    </div>
             </div>
         </div>
     </div>
