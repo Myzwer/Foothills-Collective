fbc3be06d3131e01db5bbf06eee85c5c063affb3
Adrian Garner <acgarner11@yahoo.com>
2021-08-04 12:12:38 -0400

    :construction: Added cards on desktop, still not working on mobile.

--- release.php
+++ release.php
@@ -19,27 +19,91 @@ get_header(); ?>
       rgba(0, 0, 0, 0.45)
     ), url('http://foothills-collective.local/wp-content/uploads/2021/08/Collective-Black-And-White.jpg');
      background-position: center center;
-     height: 100vh;">
+     height: 100%;">
 <div class="pt-20">
 <div class="grid grid-cols-12">
-            <div class=" lg:mx-40 bg-white p-5 col-span-12 rounded-lg mx-5">
+            <div class=" lg:mx-64 bg-white p-5 col-span-12 rounded-lg mx-5">
                 <img class="h-32 w-32 rounded-full mx-auto -mt-20" src="http://foothills-collective.local/wp-content/uploads/2021/08/Collective-Profile-Photo.jpg">
            <h2 class="text-center pt-5 text-2xl font-bold">Foothills Collective</h2>
                 <h2 class="text-center pt-2"><i class="fas fa-map-marker-alt pr-1"></i> Knoxville, TN</h2>
-                <h2 class="text-center md:mx-80 pt-3">We are a collection of artists, musicians, songwriters, and producers out of Foothills Church with the desire to create music that connects people to Jesus.</h2>
+                <h2 class="text-center xl:mx-64 md:mx-40 lg:mx-20 pt-3">We are a collection of artists, musicians, songwriters, and producers out of Foothills Church with the desire to create music that connects people to Jesus.</h2>
 
                     <div class="text-center pt-5">
-                <i class="fab fa-instagram bg-black md:p-5 p-3 rounded-full text-white"></i>
-                <i class="fab fa-facebook-f bg-black md:px-6 md:py-5 px-4 py-3 rounded-full text-white"></i>
-                <i class="fab fa-youtube bg-black md:p-5 p-3 rounded-full text-white"></i>
-                <i class="fab fa-spotify bg-black md:p-5 p-3 rounded-full text-white"></i>
-                <i class="fas fa-music bg-black md:p-5 p-3 rounded-full text-white"></i>
+                        <a href="#"><i class="fab fa-instagram bg-black md:p-5 p-3 rounded-full text-white"></i></a>
+                        <a href="#"><i class="fab fa-facebook-f bg-black md:px-6 md:py-5 px-4 py-3 rounded-full text-white"></i></a>
+                        <a href="#"><i class="fab fa-youtube bg-black md:p-5 p-3 rounded-full text-white"></i></a>
+                        <a href="#"><i class="fab fa-spotify bg-black md:p-5 p-3 rounded-full text-white"></i></a>
+                        <a href="#"><i class="fas fa-music bg-black md:p-5 p-3 rounded-full text-white"></i></a>
                     </div>
             </div>
-        </div>
+</div>
+</div>
+<!--            <div class="md:grid md:grid-cols-12">-->
+<!--            <div class="grid bg-white grid-cols-12 md:col-span-3 rounded-lg mx-5 mt-5">-->
+<!---->
+<!--                <div class="col-span-8 md:col-span-8">-->
+<!--                    <h2 class="text-center pt-3 text-sm font-bold ml-5">Even If He Doesn't</h2>-->
+<!--                    <h2 class="text-center pt-1 ml-5">Song Story</h2>-->
+<!--                </div>-->
+<!---->
+<!--                <div class=" pt-2 pl-5 col-span-3 md:col-span-4 ">-->
+<!--                    <i class="fab fa-youtube text-5xl"></i>-->
+<!--                </div>-->
+<!---->
+<!--            </div>-->
+
+
+
+
+            <div class="pt-20">
+                <div class="grid grid-cols-12 md:gap-5 pb-5 lg:mx-64">
+                    <div class="bg-white col-span-12 md:col-span-3 rounded-lg ">
+                        <i class="  text-7xl inline md:block md:text-center md:-mt-10 fab fa-youtube col-span-3"></i>
+                        <h2 class="md:text-center text-sm text-gray hidden md:block">YouTube</h2>
+                        <h2 class="md:text-center pt-3 uppercase mx-1 text-sm font-bold col-span-9">Even If He Doesn't</h2>
+                        <h2 class="md:text-center uppercase mx-1 text-sm font-bold hidden md:block">-</h2>
+                        <h2 class="text-center md:mx-4 text-xs col-span-12">Song Story Available Now</h2>
+                    </div>
+
+
+                    <div class="bg-white col-span-12 md:col-span-3 rounded-lg">
+                        <i class="text-7xl inline md:block md:text-center md:-mt-10 fab fa-youtube"></i>
+                        <h2 class="text-center text-sm text-gray hidden md:block">Spotify</h2>
+                        <h2 class="text-center pt-3 uppercase mx-1 text-sm font-bold">Even If He Doesn't</h2>
+                        <h2 class="text-center uppercase mx-1 text-sm font-bold">-</h2>
+                        <h2 class="text-center mx-4 text-xs">Song Story Available Now</h2>
+                    </div>
+
+
+                    <div class="bg-white  col-span-12 md:col-span-3 rounded-lg">
+                        <i class="text-7xl inline md:block md:text-center md:-mt-10 fab fa-youtube"></i>
+                        <h2 class="text-center text-sm text-gray hidden md:block">Apple Music</h2>
+                        <h2 class="text-center pt-3 uppercase mx-1 text-sm font-bold">Even If He Doesn't</h2>
+                        <h2 class="text-center uppercase mx-1 text-sm font-bold">-</h2>
+                        <h2 class="text-center mx-4 text-xs">Song Story Available Now</h2>
+                    </div>
+
+
+                    <div class="bg-white  col-span-12 md:col-span-3 rounded-lg">
+                        <i class="text-7xl inline md:block md:text-center md:-mt-10 fab fa-youtube"></i>
+                        <h2 class="text-center text-sm text-gray hidden md:block">Amazon</h2>
+                        <h2 class="text-center pt-3 uppercase mx-1 text-sm font-bold">Even If He Doesn't</h2>
+                        <h2 class="text-center uppercase mx-1 text-sm font-bold">-</h2>
+                        <h2 class="text-center mx-4 text-xs ">Song Story Available Now</h2>
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
+    </div>
+
     </div>
         </div>
     </div>
+
 
 
 
